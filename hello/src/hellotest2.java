67796139f2a8aae55733c61079035176a61d1364
402456 <tangchengyu.tcy@alibaba-inc.com>
2023-05-08 10:09:58 +0800

    tang05081005: new hellotest2

---
+++ hello/src/hellotest2.java
@@ -0,0 +1,9 @@
+/**
+ * @Author: TangChengYu
+ * @Date: 2023/5/8 10:09
+ */
+public class hellotest2 {
+    public static void main(String[] args) {
+        System.out.println("im hellotest2");
+    }
+}
