35a5f5130b43e4b2f3398efdab6c8ca82012bd4b
402456 <tangchengyu.tcy@alibaba-inc.com>
2023-05-08 10:38:32 +0800

    tang05081032: update1

--- hello/src/hellotest1.java
+++ hello/src/hellotest1.java
@@ -5,5 +5,6 @@
 public class hellotest1 {
     public static void main(String[] args) {
         System.out.println("im hellotest1");
+        System.out.println("im tangchengyu05081032");
     }
 }
